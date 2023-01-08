e537f1e6fa18a4b92f9e31b7dd47a521c2c035cc
sei <vanism7557@gmail.com>
2023-01-09 01:04:40 +0900

    refactor. UI transition

--- JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
@@ -10,26 +10,12 @@ import UIKit
 class StoreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
     
     @IBAction func touchApply(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showStoreView" {
-            print("showStoreView")
-//            let navigation: UINavigationController = segue.destinationViewController as! UINavigationController
-            
-//            var vc =
-        }
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    
-
     /*
     // MARK: - Navigation
 
